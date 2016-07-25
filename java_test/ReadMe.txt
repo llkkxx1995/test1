27466938ef3e9740dde6076c042637697361f3a2
llkkxx1995 <610870070@qq.com>
2016-07-25 19:02:48 +0800

    添加javad简单项目

---
+++ java_test/ReadMe.txt
@@ -0,0 +1,2 @@
+1.编译 mvn clean compile
+2.执行 mvn exec:java -Dexec.mainClass="com.hand.Try" -Dexec.args="arg0 arg1 arg2"
---
+++ java_test/src/main/java/com/hand/Try.java
@@ -0,0 +1,9 @@
+package com.hand;
+
+public class Try {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println( "Hello World2!" );
+    }
+}
